93c10a361dbbb24034d6fc144f8540e827d0c703
Colleen Que <quecolleen@gmail.com>
2022-06-14 20:38:02 -0400

    placed html-css into separate folders

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 const userRoutes = require('./users');
 const privateRoutes = require('./private');
+const path = require('path');
 
 const constructorMethod = (app) => {
-  app.use('/', userRoutes);
+  app.use('/user', userRoutes);
   app.use('/private', privateRoutes);
+  app.get('/',(req,res)=>{
+    res.sendFile(path.resolve('static/index.html'));
+  });
+  
 
   app.use('*', (req, res) => {
     res.sendStatus(404);
